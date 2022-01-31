1cd179c9142b3aab5c73f1fd71b39223138fb20a
Corey Gibb <coreygibb@Coreys-MacBook-Pro.local>
2022-01-30 20:27:59 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog`,
+    author: `Corey Gibb`,
+    contact: {
+      name: 'Corey Gibb',
+      company: 'Blogs Inc.',
+      address: 'PO Box 1234'
+    },
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
