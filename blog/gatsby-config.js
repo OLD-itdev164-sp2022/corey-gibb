fcb6f8a70c493fe3485eecf864f3cdc5888451a6
Corey Gibb <coreygibb@Coreys-MacBook-Pro.local>
2022-02-21 20:46:11 -0600

    commit1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Blog`,
@@ -17,8 +21,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: 'sq63nj735mae',
-        accessToken: 'WLEwPVFPb_yA3h8PIpnvIfVc3scHDQBdg4U6S4ltXOo'
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`
       }
     },
     {
